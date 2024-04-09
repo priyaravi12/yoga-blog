1aa1828a7f1da9e2e13fb994c9494b2ecb825c9e
priyaravi12 <priyaravi0027@gmail.com>
2024-04-09 15:24:11 +0200

    components and style sheet

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+       <div className="Homepage">
+        <h1>Homepage</h1>
+       </div>
+
+      );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+       <nav className="Navbar">
+         <h1>Yoga Blog</h1>
+         <div className="links">
+            <a href="/">Home</a>
+            <a href="/create"> Blog details</a>
+         </div>
+       </nav>
+     );
+}
+ 
+export default Navbar;
